63549a72981faf90c953f6feb8fe6ff9f3b38b34
harck5 <raaaaullopez@gmail.com>
2022-10-10 10:42:10 +0200

    Primeros scripts

---
+++ Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    //variables
+    /*blablabla
+     blablabla
+    */
+    public int playerAge = 22;
+    public float playerSpeed = 3.45f;
+    public string playerName = "harck";
+    public bool gameOver = false;
+}
