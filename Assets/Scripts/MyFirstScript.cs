0c570b55209da363d852804f6e0015cc56152d9e
harck5 <raaaaullopez@gmail.com>
2022-10-11 13:02:30 +0200

    scripts

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 public class MyFirstScript : MonoBehaviour
 {
     //variables
-    /*blablabla
-     blablabla
-    */
     public int playerAge = 22;
     public float playerSpeed = 3.45f;
     public string playerName = "harck";
     public bool gameOver = false;
+
+    public string message = "Hola mundo";
+    public int points = 256;
+
+    private void Start()
+    {
+        Debug.Log(message) ;
+        Debug.Log(points);
+        Debug.Log($"Tengo {points} puntos");
+        Debug.Log(message: string.Format("Tengo {0} puntos", points));
+        Debug.Log(message: string.Format("Hola {0} tienes {1} puntos", playerName, points));
+        Debug.Log(message: "Tengo " + points + " puntos");
+    }
 }
--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
-
+//Variable wheels num de ruedas
+[SerializeField] private int wheels = 4;
+    private void Start()
+    {
+        Debug.Log("El veiculo tiene" {0}, wheels);
+    }
 }
