8a3c08f81051031d22868946e5a683714cd7d190
harck5 <raaaaullopez@gmail.com>
2022-10-18 12:54:01 +0200

    desigualdad y mayor y menor que

--- Assets/Scripts/Operations.cs
+++ Assets/Scripts/Operations.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class Operations : MonoBehaviour
 {
-    public float x = 10;
-    public float y = 5;
-    public float result = 0;
+
+    //operaciones buleanas
+    /*public int x = 10;
+    public int y = 4;
+    private bool areEqual;
     private void Start()
     {
-        Debug.Log(result = x + y);
-        Debug.Log(result = x - y);
-        Debug.Log(result = x * y);
-        Debug.Log(result = x / y);
-        Debug.Log(result = x % y);
+        areEqual = x == y;
+        Debug.Log(areEqual);
+    }*/
+
+    /* private bool areEqual;
+
+     public string name1 = "roberto";
+     public string name2 = "alberto";
+     private bool areDiffernt;
+
+     private void Start()
+     {
+
+     areDiffernt = name1 != name2;
+         Debug.Log(areDiffernt);
+     }   */
+
+    public float x = 2.0f;
+    public float y = 0.5f;
+    private bool isGreater;
+    private void Start()
+    {
+        isGreater = x > y;
+        Debug.Log(isGreater);
+
     }
 }
+
+    //Operaciones normales
+    /*    public float x = 10;
+        public float y = 5;
+        public float result = 0;
+        private void Start()
+        {
+            Debug.Log(result = x + y);
+            Debug.Log(result = x - y);
+            Debug.Log(result = x * y);
+            Debug.Log(result = x / y);
+            Debug.Log(result = x % y);
+        }*/
+
