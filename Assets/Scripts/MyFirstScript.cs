6b18084dfe4a0ef3411e37717a0abff7f4f480de
harck5 <raaaaullopez@gmail.com>
2022-10-25 12:52:33 +0200

    if y else / vector

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -5,32 +5,34 @@ using UnityEngine;
 public class MyFirstScript : MonoBehaviour
 {
     //variables
-    public int playerAge = 22;
+    /*public int playerAge = 22;
     public float playerSpeed = 3.45f;
     public string playerName = "harck";
     public bool gameOver = false;
-
     public string message = "Hola mundo";
-    public int points = 256;
+    public int points = 256;*/
 
+    public Vector3 initialPos = new Vector3(1, 1, 1);
 
-    /*   private void Start()
+    private void Start()
        {
-           Debug.Log(message) ;
+       /*    Debug.Log(message) ;
            Debug.Log(points);
            Debug.Log($"Tengo {points} puntos");
            Debug.Log(message: string.Format("Tengo {0} puntos", points));
            Debug.Log(message: string.Format("Hola {0} tienes {1} puntos", playerName, points));
            Debug.Log(message: "Tengo " + points + " puntos");
-       }
+       
    }*/
 
-    public float x = 0;
+    /*public float x = 0;
     public float y = 2;
     private void Start()
     {
         x = x + y;
         Debug.Log(x);
 
+    }*/
+    transform.position = initialPos;
     }
 }
--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -5,21 +5,25 @@ using UnityEngine;
 public class Vehicle : MonoBehaviour
 {
 //Variable wheels num de ruedas
-[SerializeField] private int wheels = 4 ;
-[SerializeField] private string name = "car";
-[SerializeField] private bool isOn;
+        [SerializeField] private int wheels = 4 ;
+        [SerializeField] private string name = "car";
+        [SerializeField] private bool isOn;
+        [SerializeField] private float gasoline;
+        
+
+
     public string sound = "brum brum brum";
     private void Start()
     {
-        //Debug.Log(message: string.Format("Tu vehiculo es {0} y tiene {1} ruedas" , name , wheels)) ;
-        /*if (isOn == true)
+        /*Debug.Log(message: string.Format("Tu vehiculo es {0} y tiene {1} ruedas" , name , wheels)) ;
+        if (isOn == true)
         {
             Debug.Log($"El {name} esta encendido");
         }
         else
         {
             Debug.Log($"La {name} esta apagado");
-        }*/
+        }
         if (isOn == false)
         {
             Debug.Log($"{name} esta apagado");
@@ -32,6 +36,34 @@ public class Vehicle : MonoBehaviour
         else
         {
             Debug.Log($"{name} en marcha");
+        }
+        if (isOn == true)
+        {
+            if (gasoline < 10) {
+                Debug.Log(message: $"{name} le queda {gasoline} gasolina");
+            }
+            else
+            {
+            Debug.Log(message: $"{name} esta lleno");
+            } 
+        }
+        else 
+        {
+            Debug.Log(message: $"{name} esta apagado");
+        }*/
+        if (isOn == true && gasoline < 10)
+        {
+            Debug.Log(message: $"{name} esta en marcha y queda {gasoline} litros de gasolinas");
+        }
+
+        else if (gasoline == 10)
+            {
+                Debug.Log(message: $"{name} esta en marcha y esta lleno");
+            }
+       
+        else
+        {
+            Debug.Log(message: $"{name} esta apagado y tiene {gasoline} gasolina");
         }
     }
     
